421d59c85b9726b6ea3d1a16d8c5fde49e4307e0
dapengChen <202245220@qq.com>
2018-12-16 14:55:36 +0800

    feat

---
+++ src/pages/searchAdvert/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="search-page">广告搜索页</div>
+</template>
+<script>
+export default {
+  name: 'search'
+}
+</script>
+<style lang="scss" scoped>
+.search-page {}
+</style>
+
+
--- src/pages/transactions/index.vue
+++ src/pages/transactions/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="transactions-page">明细</div>
+  <div class="transactions-page">
+    <button @click="navigateToPage('transactionDetail')">明细list</button>
+  </div>
 </template>
 <script>
 export default {
-  name: 'transactions'
-}
+  name: "transactions",
+  methods: {
+    navigationToPage(path) {
+      const url = `../${path}/main`;
+      wx.navigateTo({ url });
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-.transactions-page {}
+.transactions-page {
+}
 </style>
